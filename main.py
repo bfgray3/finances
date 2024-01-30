5ed5d2339e258a2e2da0ae90cdaac6da59bf0117
Bernie Gray <bfgray3@gmail.com>
2024-01-29 21:07:02 -0500

    have df of date and decimals

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import polars as pl
 
-print(pl.__version__)
+
+def read_data() -> pl.DataFrame:
+    return pl.read_csv("balance-sheet.csv").select(
+        pl.col("Date").str.to_date("%m/%d/%Y"),
+        pl.exclude("Date", "Total", "Change", "Notes")
+        .str.replace_all("[$,]", "")
+        .str.to_decimal(),
+    )
+
+
+print(read_data())
+print(read_data().columns)
