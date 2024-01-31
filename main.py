92f5540d63b819a9869eecb5649b0fc7fb2e2ed9
Bernie Gray <bfgray3@gmail.com>
2024-01-31 06:12:35 -0500

    main function

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
+import argparse
 import json
+from typing import Sequence, TypedDict
 
 import polars as pl
 import seaborn.objects as so
 
-with open("names.json") as f:
-    names = json.load(f)
+
+class NamesDict(TypedDict):
+    assets: list[str]
+    liabilities: list[str]
 
 
-def read_data() -> pl.DataFrame:
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv", default="balance-sheet.csv")
+    parser.add_argument("--names", default="names.json")
+    args = parser.parse_args(argv)
+
+    with open("names.json") as f:
+        names = json.load(f)
+
+    data = read_data(args.csv, names)
+    so.Plot(data, "Date", "Total").add(so.Line()).save("total")
+    so.Plot(data, "Date", "Change").add(so.Line()).save("change")
+
+    (
+        so.Plot(
+            data.melt(
+                id_vars="Date",
+                value_vars=names["assets"],
+            ),
+            "Date",
+            "value",
+            color="variable",
+        ).add(so.Area(), so.Stack())
+    ).save("stacked")
+    return 0
+
+
+def read_data(csv: str, names: NamesDict) -> pl.DataFrame:
     return (
-        pl.read_csv("balance-sheet.csv")
+        pl.read_csv(csv)
         .select(
             pl.col("Date").str.to_date("%m/%d/%Y"),
             pl.exclude("Date", "Total", "Change", "Notes")
@@ -28,18 +59,4 @@ def read_data() -> pl.DataFrame:
 
 
 if __name__ == "__main__":
-    data = read_data()
-    so.Plot(data, "Date", "Total").add(so.Line()).save("total")
-    so.Plot(data, "Date", "Change").add(so.Line()).save("change")
-
-    (
-        so.Plot(
-            data.melt(
-                id_vars="Date",
-                value_vars=names["assets"],
-            ),
-            "Date",
-            "value",
-            color="variable",
-        ).add(so.Area(), so.Stack())
-    ).save("stacked")
+    raise SystemExit(main())
