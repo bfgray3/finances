ea19a1c8717863b991ac3194641bb677565a2b72
Bernie Gray <bfgray3@gmail.com>
2024-01-30 22:13:02 -0500

    calculate monthly change

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import polars as pl
 
 
 def read_data() -> pl.DataFrame:
-    return pl.read_csv("balance-sheet.csv").select(
-        pl.col("Date").str.to_date("%m/%d/%Y"),
-        pl.exclude("Date", "Total", "Change", "Notes")
-        .str.replace_all("[$,]", "")
-        .str.to_decimal(),
+    return (
+        pl.read_csv("balance-sheet.csv")
+        .select(
+            pl.col("Date").str.to_date("%m/%d/%Y"),
+            pl.exclude("Date", "Total", "Change", "Notes")
+            .str.replace_all("[$,]", "")
+            .str.to_decimal(),
+        )
+        .with_columns(
+            Total=pl.sum_horizontal(pl.exclude("StudentLoans", "CreditCards", "Date"))
+            - pl.sum_horizontal(pl.col(["StudentLoans", "CreditCards"]))
+        )
+        .with_columns(
+            PctChange=pl.col("Total").pct_change().alias("PctChange"),
+            Change=pl.col("Total").diff(),
+        )
     )
 
 
