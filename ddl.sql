264630b09889c9100b7c5c0311bb69525bf36ca6
Bernie Gray <bfgray3@gmail.com>
2024-09-08 19:02:28 -0400

    add mysql ddl

---
+++ ddl.sql
@@ -0,0 +1,16 @@
+create table if not exists amounts (
+  id mediumint not null auto_increment,
+  day date not null,
+  amount decimal(10,2) not null,
+  class_id smallint not null,
+  primary key (id),
+  foreign key (class_id) references classes(id)
+);
+
+
+create table if not exists classes (
+  id smallint not null auto_increment,
+  name varchar(255) not null,
+  is_asset bool not null,
+  primary key (id)
+);
