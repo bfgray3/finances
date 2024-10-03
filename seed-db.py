0b1d77254f884dff9ab2a51fca462cf4358fe5ba
Bernie Gray <bfgray3@gmail.com>
2024-10-02 21:28:02 -0400

    start working on adding things to the db

--- seed-db.py
+++ seed-db.py
@@ -1,3 +1,5 @@
+import json
+
 import polars as pl
 
 df = (
@@ -14,6 +16,33 @@ assert df.null_count().select(s=pl.sum_horizontal(pl.all())).row(0, named=True) 
 }
 
 print(df)
+
+with open("names.json") as f:
+    asset_indicators = json.load(f)
+
+
+""" from the docs
+from databases import Database
+database = Database('sqlite+aiosqlite:///example.db')
+await database.connect()
+
+query = "INSERT INTO HighScores(name, score) VALUES (:name, :score)"
+values = [
+    {"name": "Daisy", "score": 92},
+    {"name": "Neil", "score": 87},
+    {"name": "Carol", "score": 43},
+]
+await database.execute_many(query=query, values=values)
+
+query = "SELECT * FROM HighScores"
+rows = await database.fetch_all(query=query)
+print('High Scores:', rows)
+"""
+
+for col in set(df.columns) - {"Date"}:
+    is_asset = col in asset_indicators["assets"]
+    print(col, is_asset)
+
 
 for row in df.iter_rows(named=True):
     # TODO: insert into the various tables
