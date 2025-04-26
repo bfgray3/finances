abc33b9c2c3f4ada901f2269e44632bb702d3d50
Bernie Gray <bfgray3@gmail.com>
2025-04-25 22:32:57 -0400

    add comments table

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -13,11 +13,20 @@ create table if not exists finances.dates (
 
 create table if not exists finances.amounts (
   id mediumint not null auto_increment,
-  day_id date not null,
+  day_id mediumint not null,
   amount decimal(10,2) not null,
   class_id smallint not null,
   primary key (id),
   foreign key (class_id) references classes(id),
   foreign key (day_id) references dates(id),
   unique (day_id, class_id)
+);
+
+create table if not exists finances.comments (
+  id mediumint not null auto_increment,
+  day_id mediumint not null,
+  comments varchar(255) not null,
+  primary key (id),
+  foreign key (day_id) references dates(id),
+  unique (day_id)
 );
